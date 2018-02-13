2f22cb06b93e41dbae831ad908b63d4b7b0f668c
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-02-13 21:20:09 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Capacitor, Plugins } from '@capacitor/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  image: SafeResourceUrl;
 
+  constructor(public navCtrl: NavController, private sanitizer: DomSanitizer) {
+
+    console.log("Capacitor.platform: ", Capacitor.platform);
   }
+
+  takePicture() {
+    console.log("takePicture");
+
+    const { Camera } = Plugins;
+
+    const image = /*await*/ Camera.getPhoto({
+        quality: 100,
+        resultType: 'base64'
+    }).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+        console.log('Sorry pal, not going to happen');
+    });
+
+    this.image = this.sanitizer.bypassSecurityTrustResourceUrl(image && ('data:image/jpeg;base64,' + image['base64_data']));
+
+}
 
 }
